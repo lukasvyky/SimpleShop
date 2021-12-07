5c24cc78ba4610c09b076cb17d3a3a7955f4dc63
lukasvyky <lucas.vykydal@outlook.com>
2021-12-07 23:19:44 +0100

    Add Vue.js with basic examples

---
+++ Shop.UI/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shop.Application.Products;
+using Shop.Database;
+
+namespace Shop.UI.Pages
+{
+    public class AdminIndexModel : IndexModel
+    {
+        public AdminIndexModel(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Shop.UI/wwwroot/js/admin/main.js
@@ -0,0 +1,20 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        price: 0,
+        showPrice: true
+    },
+    computed: {
+        formatPrice: function () {
+            return "CZK" + this.price;
+        }  
+    },
+    methods: {
+        togglePrice: function () {
+            this.showPrice = !this.showPrice;
+        },
+        alert: function (m) {
+            alert(m);
+        }
+    }
+});
