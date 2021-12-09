9546587c0d6ee452e7b179f9cf5d66b93889d099
lukasvyky <lucas.vykydal@outlook.com>
2021-12-09 17:29:01 +0100

    Create seperate layout for admin page and remove js component

--- Shop.UI/wwwroot/js/admin/products.js
+++ Shop.UI/wwwroot/js/admin/products.js
@@ -1,65 +1,16 @@
-﻿Vue.component('product-manager', {
-    template: `<div>
-            <div v-if="!editing">
-                <table class="table">
-                    <tr>
-                        <th>Id</th>
-                        <th>Product</th>
-                        <th>Value</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                    <tr v-for="(product, index) in products">
-                        <td>{{product.id}}</td>
-                        <td>{{product.name}}</td>
-                        <td>{{product.value}}</td>
-                        <td> <a @click="editProduct(product.id, index)">Edit</a></td>
-                        <td> <a @click="deleteProduct(product.id, index)">Remove</a></td>
-                    </tr>
-                </table>
-                <button class="button" @click="newProduct">Add new product</button>
-            </div>
-
-            <div v-else>
-                <div class="field">
-                    <label class="label">Product name</label>
-                    <div class="control">
-                        <input class="input" v-model="productModel.name" />
-                    </div>
-
-                    <div class="field">
-                        <label class="label">Product description</label>
-                        <div class="control">
-                            <input class="input" v-model="productModel.description" />
-                        </div>
-                    </div>
-
-                    <div class="field">
-                        <label class="label">Product value</label>
-                        <div class="control">
-                            <input class="input" v-model="productModel.value" />
-                        </div>
-                    </div>
-                </div>
-
-                <button class="button is-success" @click="createProduct" v-if="!productModel.id">Create Product</button>
-                <button class="button is-warning" @click="updateProduct" v-else>Update Product</button>
-                <button class="button" @click="cancel">Cancel</button>
-            </div>
-       </div>`,
-    data() {
-        return {
-            editing: false,
-            objectIndex: 0,
-            loading: false,
-            productModel: {
-                id: 0,
-                name: "Product Name",
-                description: "Product Description",
-                value: 1
-            },
-            products: []
-        }
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        editing: false,
+        objectIndex: 0,
+        loading: false,
+        productModel: {
+            id: 0,
+            name: "Product Name",
+            description: "Product Description",
+            value: 1
+        },
+        products: []
     },
     computed: {
     },
