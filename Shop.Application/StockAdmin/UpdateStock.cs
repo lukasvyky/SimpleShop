eee8ce7a1ee3584854bb062068025216841058fe
lukasvyky <lucas.vykydal@outlook.com>
2021-12-09 18:21:30 +0100

    Add Get and update stock functionality

---
+++ Shop.Application/StockAdmin/UpdateStock.cs
@@ -0,0 +1,61 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.StockAdmin
+{
+    public class UpdateStock
+    {
+        private ApplicationDbContext Context { get; }
+        public UpdateStock(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+
+        public async Task<Response> Do(Request request)
+        {
+            var stockList = new List<Stock>();
+
+            foreach (var stock in request.Stock)
+            {
+                stockList.Add(new Stock
+                {
+                    Id = stock.Id,
+                    Description = stock.Description,
+                    Qty = stock.Qty,
+                    ProductId = stock.ProductId
+                });
+            }
+
+            Context.UpdateRange(stockList);
+            await Context.SaveChangesAsync();
+
+            return new Response
+            {
+                Stock = request.Stock
+            };
+        }
+
+        public class StockViewModel
+        {
+            public int Id { get; set; }
+            public int ProductId { get; set; }
+            public string Description { get; set; }
+            public int Qty { get; set; }
+        }
+
+        public class Request
+        {
+            public IEnumerable<StockViewModel> Stock { get; set; }
+        }
+
+        public class Response
+        {
+            public IEnumerable<StockViewModel> Stock { get; set; }
+        }
+    }
+}
