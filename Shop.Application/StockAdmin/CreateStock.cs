5c859e9cb0b6dce7b067d8312a97f1cb4ff52b01
lukasvyky <lucas.vykydal@outlook.com>
2021-12-09 17:58:19 +0100

    Add createStock functionality

---
+++ Shop.Application/StockAdmin/CreateStock.cs
@@ -0,0 +1,53 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.StockAdmin
+{
+    public class CreateStock
+    {
+        private ApplicationDbContext Context { get; }
+        public CreateStock(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+
+        public async Task<Response> Do(Request request)
+        {
+            var stockToSave = new Stock
+            {
+                ProductId = request.ProductId,
+                Description = request.Description,
+                Qty = request.Qty
+            };
+            
+            Context.Stocks.Add(stockToSave);
+            await Context.SaveChangesAsync();
+
+            return new Response
+            {
+                Id = stockToSave.Id,
+                Description = stockToSave.Description,
+                Qty = stockToSave.Qty
+            };
+        }
+
+        public class Request
+        {
+            public int ProductId { get; set; }
+            public string Description { get; set; }
+            public int Qty { get; set; }  
+        }
+        public class Response
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public int Qty { get; set; }
+        }
+
+    }
+}
