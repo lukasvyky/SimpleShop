8244f2f6a748370b2f3c732119c9347c8f8f36a8
lukasvyky <lucas.vykydal@outlook.com>
2021-12-09 18:03:13 +0100

    Add deleteStock functionality

---
+++ Shop.Application/StockAdmin/DeleteStock.cs
@@ -0,0 +1,27 @@
+﻿using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.StockAdmin
+{
+    public class DeleteStock
+    {
+        private ApplicationDbContext Context { get; }
+        public DeleteStock(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+
+        public async Task<bool> Do(int id)
+        {
+            var stockToRemove = Context.Stocks.Find(id);
+            Context.Stocks.Remove(stockToRemove);
+            await Context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
