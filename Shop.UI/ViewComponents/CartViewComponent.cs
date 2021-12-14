fdbccf68b0b9bee086e70059a41b5eec162a69c1
lukasvyky <lucas.vykydal@outlook.com>
2021-12-14 19:58:05 +0100

    Add cart as viewComponent

---
+++ Shop.UI/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Cart;
+using Shop.Database;
+
+namespace Shop.UI.ViewComponents
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private ApplicationDbContext Context { get; }
+
+        public CartViewComponent(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+
+        public IViewComponentResult Invoke(string view = "Default")
+        {
+            return View(view,new GetCart(HttpContext.Session, Context).Do());
+        }
+    }
+}
